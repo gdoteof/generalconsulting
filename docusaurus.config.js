5dc6b3232aa64f2e693df2cc0159364cbd27eefe
gdoteof <gdoteof@gmail.com>
2023-06-28 12:17:45 -0400

    thanks dale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'General Consulitng',
+  title: 'General Consulting',
   tagline: 'First class solutions, derived from first principles',
   favicon: 'img/favicon.ico',
 
